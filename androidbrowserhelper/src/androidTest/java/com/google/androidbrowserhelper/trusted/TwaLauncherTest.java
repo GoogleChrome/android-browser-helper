e7484f2c539aaf6eefbde4f6af3e6acadafa7552
Peter E Conn <peconn@google.com>
2019-08-14 10:00:13 +0100

    Nits

--- androidbrowserhelper/src/androidTest/java/com/google/androidbrowserhelper/trusted/TwaLauncherTest.java
+++ androidbrowserhelper/src/androidTest/java/com/google/androidbrowserhelper/trusted/TwaLauncherTest.java
@@ -21,7 +21,6 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.mock;
--- androidbrowserhelper/src/main/java/com/google/androidbrowserhelper/trusted/ChromeLegacyUtils.java
+++ androidbrowserhelper/src/main/java/com/google/androidbrowserhelper/trusted/ChromeLegacyUtils.java
@@ -1,9 +1,19 @@
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package com.google.androidbrowserhelper.trusted;
 
-import android.content.Context;
 import android.content.pm.PackageManager;
-import android.content.res.Resources;
-import android.widget.Toast;
 
 import java.util.Arrays;
 import java.util.List;
@@ -28,8 +38,6 @@ import androidx.browser.customtabs.CustomTabsService;
  *
  * We hope to remove this class once the versions of Chrome that require special behaviour are no
  * longer in wide use.
- *
- * TODO: Add resource.
  */
 public class ChromeLegacyUtils {
     private static final String CHROME_LOCAL_BUILD_PACKAGE = "com.google.android.apps.chrome";

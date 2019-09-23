98ec159d906228ff1713aa0cda364025ebc48b79
Peter E Conn <peconn@google.com>
2019-09-23 14:22:05 +0100

    Add licenses, get rid of .idea.

--- demo/src/main/java/com/google/androidbrowserhelper/DemoListActivity.java
+++ demo/src/main/java/com/google/androidbrowserhelper/DemoListActivity.java
@@ -1,6 +1,18 @@
-package com.google.androidbrowserhelper;
+// Copyright 2019 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-import androidx.appcompat.app.AppCompatActivity;
+package com.google.androidbrowserhelper;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,6 +20,8 @@ import android.view.View;
 
 import com.google.androidbrowserhelper.demo.R;
 import com.google.androidbrowserhelper.launchtwa.LaunchTwaActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class DemoListActivity extends AppCompatActivity {
     @Override
--- demo/src/main/java/com/google/androidbrowserhelper/launchtwa/LaunchTwaActivity.java
+++ demo/src/main/java/com/google/androidbrowserhelper/launchtwa/LaunchTwaActivity.java
@@ -1,14 +1,18 @@
+// Copyright 2019 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package com.google.androidbrowserhelper.launchtwa;
-
-import static androidx.browser.customtabs.CustomTabsIntent.COLOR_SCHEME_SYSTEM;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.browser.customtabs.CustomTabColorSchemeParams;
-import androidx.browser.customtabs.CustomTabsClient;
-import androidx.browser.customtabs.CustomTabsIntent;
-import androidx.browser.customtabs.CustomTabsServiceConnection;
-import androidx.browser.customtabs.CustomTabsSession;
-import androidx.browser.trusted.TrustedWebActivityIntentBuilder;
 
 import android.content.ComponentName;
 import android.content.Intent;
@@ -24,6 +28,12 @@ import com.google.androidbrowserhelper.trusted.TwaProviderPicker;
 
 import java.util.Arrays;
 import java.util.List;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.browser.customtabs.CustomTabsClient;
+import androidx.browser.customtabs.CustomTabsServiceConnection;
+import androidx.browser.customtabs.CustomTabsSession;
+import androidx.browser.trusted.TrustedWebActivityIntentBuilder;
 
 public class LaunchTwaActivity extends AppCompatActivity {
     private static final Uri LAUNCH_URI =

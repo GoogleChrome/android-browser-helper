9c7ad767599203a0c2cd9af144e8f3a30fbc0fb5
Peter E Conn <peconn@google.com>
2019-11-27 16:13:21 +0000

    Provide an Intent to the browser to focus the TWA.

--- androidbrowserhelper/src/main/java/com/google/androidbrowserhelper/trusted/TwaLauncher.java
+++ androidbrowserhelper/src/main/java/com/google/androidbrowserhelper/trusted/TwaLauncher.java
@@ -194,6 +194,7 @@ public class TwaLauncher {
             @Nullable Runnable completionCallback) {
         Log.d(TAG, "Launching Trusted Web Activity.");
         Intent intent = builder.build(mSession);
+        FocusActivity.addToIntent(intent, mContext);
         ContextCompat.startActivity(mContext, intent, null);
         // Remember who we connect to as the package that is allowed to delegate notifications
         // to us.

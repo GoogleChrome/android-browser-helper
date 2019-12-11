d3737e9c6bf7f3c19669882c77ffc87b42fa9bde
Andre Bandarra <andreban@google.com>
2019-12-11 18:31:14 +0000

    Adds a twa-webview-fallback demo
    Adds an almost empty demo project, to be used to build a proper
    WebView fallback demo.

---
+++ demos/twa-webview-fallback/src/main/java/com/google/browser/examples/twawebviewfallback/TwaLauncherActivity.java
@@ -0,0 +1,6 @@
+package com.google.browser.examples.twawebviewfallback;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TwaLauncherActivity extends AppCompatActivity {
+}
---
+++ demos/twa-webview-fallback/src/main/java/com/google/browser/examples/twawebviewfallback/WebViewFallbackActivity.java
@@ -0,0 +1,14 @@
+package com.google.browser.examples.twawebviewfallback;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WebViewFallbackActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view_fallback);
+    }
+}

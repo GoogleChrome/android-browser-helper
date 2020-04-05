86a238a846e7317a094cd3de5c7bbffca4819d58
Peter Romianowski <pero@flunder.io>
2020-04-05 12:51:42 +0200

    Adapted code to API changes
    This code did not work in the most current master branch, too.

--- androidbrowserhelper/src/androidTest/java/com/google/androidbrowserhelper/trusted/splashscreens/PwaWrapperSplashScreenStrategyTest.java
+++ androidbrowserhelper/src/androidTest/java/com/google/androidbrowserhelper/trusted/splashscreens/PwaWrapperSplashScreenStrategyTest.java
@@ -155,17 +155,17 @@ public class PwaWrapperSplashScreenStrategyTest {
         strategy.configureTwaBuilder(builder, mSession, latch::countDown);
         assertTrue(latch.await(3, TimeUnit.SECONDS));
 
-        Intent intent = builder.build(mSession);
+        Intent intent = builder.build(mSession).getIntent();
         Bundle bundle = intent.getBundleExtra(EXTRA_SPLASH_SCREEN_PARAMS);
 
-        assertEquals(bgColor, bundle.getInt(SplashScreenParamKey.BACKGROUND_COLOR));
-        assertEquals(scaleType.ordinal(), bundle.getInt(SplashScreenParamKey.SCALE_TYPE));
-        assertEquals(fadeOutDuration, bundle.getInt(SplashScreenParamKey.FADE_OUT_DURATION_MS));
+        assertEquals(bgColor, bundle.getInt(SplashScreenParamKey.KEY_BACKGROUND_COLOR));
+        assertEquals(scaleType.ordinal(), bundle.getInt(SplashScreenParamKey.KEY_SCALE_TYPE));
+        assertEquals(fadeOutDuration, bundle.getInt(SplashScreenParamKey.KEY_FADE_OUT_DURATION_MS));
 
         float[] matrixValues = new float[9];
         matrix.getValues(matrixValues);
         assertArrayEquals(matrixValues, bundle.getFloatArray(
-                SplashScreenParamKey.IMAGE_TRANSFORMATION_MATRIX), 1e-3f);
+                SplashScreenParamKey.KEY_IMAGE_TRANSFORMATION_MATRIX), 1e-3f);
     }
 
     @Test

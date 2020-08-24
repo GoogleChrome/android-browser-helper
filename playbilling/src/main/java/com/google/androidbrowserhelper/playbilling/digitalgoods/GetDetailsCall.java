1a92b317cd62729995838524dbb3164c5a82d892
Peter E Conn <conn.peter@gmail.com>
2020-08-24 14:47:18 +0100

    License and rewiring.

--- playbilling/src/main/java/com/google/androidbrowserhelper/playbilling/digitalgoods/GetDetailsCall.java
+++ playbilling/src/main/java/com/google/androidbrowserhelper/playbilling/digitalgoods/GetDetailsCall.java
@@ -17,8 +17,10 @@ package com.google.androidbrowserhelper.playbilling.digitalgoods;
 import android.os.Bundle;
 import android.os.Parcelable;
 
+import com.android.billingclient.api.AcknowledgePurchaseParams;
 import com.android.billingclient.api.BillingResult;
 import com.android.billingclient.api.SkuDetails;
+import com.google.androidbrowserhelper.playbilling.provider.BillingWrapper;
 
 import java.util.Arrays;
 import java.util.List;
@@ -37,12 +39,12 @@ public class GetDetailsCall {
     static final String RESPONSE_GET_DETAILS_RESPONSE_CODE = "getDetails.responseCode";
     static final String RESPONSE_GET_DETAILS_DETAILS_LIST = "getDetails.detailsList";
 
-    public final List<String> itemIds;
+    private final List<String> mItemIds;
     private final DigitalGoodsCallback mCallback;
 
-    private GetDetailsCall(String[] itemIds, DigitalGoodsCallback callback) {
-        this.itemIds = Arrays.asList(itemIds);
-        this.mCallback = callback;
+    private GetDetailsCall(List<String> itemIds, DigitalGoodsCallback callback) {
+        mItemIds = itemIds;
+        mCallback = callback;
     }
 
     /** Creates this class from a {@link Bundle}, returns {@code null} if the Bundle is invalid. **/
@@ -54,42 +56,31 @@ public class GetDetailsCall {
         String[] itemIds = args.getStringArray(PARAM_GET_DETAILS_ITEM_IDS);
         if (itemIds == null) return null;
 
-        return new GetDetailsCall(itemIds, callback);
+        return new GetDetailsCall(Arrays.asList(itemIds), callback);
     }
 
     /** Calls the callback provided in the constructor with serialized forms of the parameters. */
-    private void respond(int responseCode, ItemDetails... details) {
+    private void respond(BillingResult code, List<SkuDetails> detailsList) {
+        Parcelable[] parcelables = new Parcelable[detailsList.size()];
+
+        int index = 0;
+        for (SkuDetails details : detailsList) {
+            parcelables[index] = ItemDetails.create(details).toBundle();
+        }
+
         Bundle args = new Bundle();
-        args.putInt(RESPONSE_GET_DETAILS_RESPONSE_CODE, responseCode);
-        args.putParcelableArray(RESPONSE_GET_DETAILS_DETAILS_LIST, toParcelableArray(details));
+        args.putInt(RESPONSE_GET_DETAILS_RESPONSE_CODE, code.getResponseCode());
+        args.putParcelableArray(RESPONSE_GET_DETAILS_DETAILS_LIST, parcelables);
         mCallback.run(RESPONSE_GET_DETAILS, args);
     }
 
-    /** Calls the callback provided in the constructor with serialized forms of the parameters. */
-    public void respond(BillingResult code, List<SkuDetails> details) {
-        int responseCode = code.getResponseCode();
-        ItemDetails[] itemDetails = new ItemDetails[details.size()];
-
-        int index = 0;
-        for (SkuDetails skuDetails : details) {
-            itemDetails[index++] = ItemDetails.create(skuDetails);
-        }
-
-        respond(responseCode, itemDetails);
-    }
-
-    private static Parcelable[] toParcelableArray(ItemDetails... itemDetails) {
-        Parcelable[] out = new Parcelable[itemDetails.length];
-
-        for (int i = 0; i < itemDetails.length; i++) {
-            out[i] = itemDetails[i].toBundle();
-        }
-
-        return out;
+    /** Calls the appropriate method on {@link BillingWrapper}. */
+    public void call(BillingWrapper billing) {
+        billing.querySkuDetails(mItemIds, this::respond);
     }
 
     /** Creates a Bundle that can be used with {@link #create}. For testing. */
-    public static Bundle createBundleForTesting(String... itemIds) {
+    static Bundle createBundleForTesting(String... itemIds) {
         Bundle bundle = new Bundle();
         bundle.putStringArray(PARAM_GET_DETAILS_ITEM_IDS, itemIds);
         return bundle;

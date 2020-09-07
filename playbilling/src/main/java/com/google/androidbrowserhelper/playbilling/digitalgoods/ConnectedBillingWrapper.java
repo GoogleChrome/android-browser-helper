47ecf94578d97abb79f4e3647f618e376ef57ed6
Peter E Conn <conn.peter@gmail.com>
2020-09-07 11:47:32 +0100

    Use Runnable

--- playbilling/src/main/java/com/google/androidbrowserhelper/playbilling/digitalgoods/ConnectedBillingWrapper.java
+++ playbilling/src/main/java/com/google/androidbrowserhelper/playbilling/digitalgoods/ConnectedBillingWrapper.java
@@ -1,3 +1,17 @@
+// Copyright 2020 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package com.google.androidbrowserhelper.playbilling.digitalgoods;
 
 import android.app.Activity;
@@ -25,17 +39,13 @@ public class ConnectedBillingWrapper implements BillingWrapper {
     private static final int CONNECTED = 2;
     private int mState = NOT_CONNECTED;
 
-    private interface Callback {
-        void run();
-    }
-
-    private final List<Callback> mPendingCallbacks = new ArrayList<>();
+    private final List<Runnable> mPendingCallbacks = new ArrayList<>();
 
     public ConnectedBillingWrapper(BillingWrapper mInner) {
         this.mInner = mInner;
     }
 
-    private void execute(Callback callback) {
+    private void execute(Runnable callback) {
         if (mState == CONNECTED) {
             callback.run();
             return;
@@ -51,7 +61,7 @@ public class ConnectedBillingWrapper implements BillingWrapper {
             public void onBillingSetupFinished(BillingResult billingResult) {
                 mState = CONNECTED;
 
-                for (Callback callback : mPendingCallbacks) {
+                for (Runnable callback : mPendingCallbacks) {
                     callback.run();
                 }
 

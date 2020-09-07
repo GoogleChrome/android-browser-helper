63dd7bb050ddd97b30c6ade97afe35811ad9a66b
Andre Bandarra <andreban@google.com>
2020-09-07 09:22:10 +0100

    Removes androidx.appcompat dependency
    - Removes dependency from build.grade.
    - MainActivity now extends Activity.
    - LauncherActivity uses `@android:style/Theme.Translucent.NoTitleBar` theme.

--- demos/twa-web-share-target/src/main/java/com/google/androidbrowserhelper/webshare/MainActivity.java
+++ demos/twa-web-share-target/src/main/java/com/google/androidbrowserhelper/webshare/MainActivity.java
@@ -14,13 +14,12 @@
 
 package com.google.androidbrowserhelper.webshare;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

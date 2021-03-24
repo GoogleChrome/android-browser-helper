1bb7b6ea842d144a82c5185c8559c151dd8aa567
demianrenzulli <demian77@gmail.com>
2021-03-24 14:57:42 -0400

    Changes based on andreban comments

--- demos/twa-offline-first/src/main/java/com/google/androidbrowserhelper/demos/twa_offline_first/OfflineFirstTWALauncherActivity.java
+++ demos/twa-offline-first/src/main/java/com/google/androidbrowserhelper/demos/twa_offline_first/OfflineFirstTWALauncherActivity.java
@@ -1,3 +1,17 @@
+// Copyright 2020 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package com.google.androidbrowserhelper.demos.twa_offline_first;
 
 import com.google.androidbrowserhelper.trusted.LauncherActivity;
@@ -13,7 +27,7 @@ public class OfflineFirstTWALauncherActivity extends LauncherActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.displayScreen();
+        this.tryLaunchTwa();
     }
 
     @Override
@@ -21,21 +35,21 @@ public class OfflineFirstTWALauncherActivity extends LauncherActivity {
         return false;
     }
 
-    private void displayScreen() {
-        if(isOnline()) {
+    private void tryLaunchTwa() {
+        if (isOnline()) {
             launchTwa();
         } else {
-            launchCustomOfflineScreen();
+            renderOfflineFallback();
         }
     }
 
-    private void launchCustomOfflineScreen() {
+    private void renderOfflineFallback() {
         setContentView(R.layout.activity_offline_first_twa);
 
         final Button retryBtn = this.findViewById(R.id.retry_btn);
         retryBtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                displayScreen();
+                tryLaunchTwa();
             }
         });
     }

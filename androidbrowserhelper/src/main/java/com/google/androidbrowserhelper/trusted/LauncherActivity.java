48826e34c9f1b577a841dcc93595532cdb1bfb70
Ella Ge <eirage@chromium.org>
2023-05-03 14:49:59 +0000

    Remove QualityEnforcer

--- androidbrowserhelper/src/main/java/com/google/androidbrowserhelper/trusted/LauncherActivity.java
+++ androidbrowserhelper/src/main/java/com/google/androidbrowserhelper/trusted/LauncherActivity.java
@@ -274,7 +274,7 @@ public class LauncherActivity extends Activity {
     }
 
     protected CustomTabsCallback getCustomTabsCallback() {
-        return new QualityEnforcer();
+        return null;
     }
 
     protected TwaLauncher createTwaLauncher() {

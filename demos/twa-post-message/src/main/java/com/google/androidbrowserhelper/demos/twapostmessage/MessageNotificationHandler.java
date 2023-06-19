7cbece9470e7f585fee9e588b66d9ed6408b2a9b
Sayed <elabadysayed@google.com>
2023-06-19 16:12:09 +0100

    Improve the demo code based on the PR comments

--- demos/twa-post-message/src/main/java/com/google/androidbrowserhelper/demos/twapostmessage/MessageNotificationHandler.java
+++ demos/twa-post-message/src/main/java/com/google/androidbrowserhelper/demos/twapostmessage/MessageNotificationHandler.java
@@ -1,4 +1,19 @@
 package com.google.androidbrowserhelper.demos.twapostmessage;
+/*
+ *    Copyright 2023 Google LLC
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
@@ -11,40 +26,42 @@ import androidx.core.app.NotificationManagerCompat;
 
 public class MessageNotificationHandler {
 
-  private final static String CHANNEL_ID = "TWA-postmessage-channel-id";
+    private final static String CHANNEL_ID = "channel_id";
 
-
-  public static void showNotificationWithMessage(Context context, String message) {
-    Intent intent = new Intent();
-      intent.setAction(PostMessageBroadcastReceiver.POST_MESSAGE_ACTION);
-
-    PendingIntent pendingIntent =
-        PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_IMMUTABLE);
-
-    NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID)
-        .setSmallIcon(R.drawable.ic_launcher_background)
-        .setContentTitle("Received a message")
-        .setContentText(message)
-        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-        .addAction(R.drawable.ic_launcher_background, "Reply back", pendingIntent)
-        .setAutoCancel(true);
-
-    NotificationManagerCompat.from(context).notify(1, builder.build());
-
-  }
-
-  public static void createNotificationChannelIfNeeded(Context context) {
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-      String name = "TWA-postMessage-Demo";
-      String descriptionText = "A channel to send post message demo notification";
-      int importance = NotificationManager.IMPORTANCE_HIGH;
-      NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
-      channel.setDescription(descriptionText);
-
-      NotificationManager notificationManager =
-          (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-
-      notificationManager.createNotificationChannel(channel);
+    private MessageNotificationHandler() {
     }
-  }
+
+    public static void showNotificationWithMessage(Context context, String message) {
+        Intent intent = new Intent();
+        intent.setAction(PostMessageBroadcastReceiver.POST_MESSAGE_ACTION);
+
+        PendingIntent pendingIntent =
+            PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_IMMUTABLE);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .setContentTitle("Received a message")
+            .setContentText(message)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .addAction(R.drawable.ic_launcher_background, "Reply back", pendingIntent)
+            .setAutoCancel(true);
+
+        NotificationManagerCompat.from(context).notify(1, builder.build());
+
+    }
+
+    public static void createNotificationChannelIfNeeded(Context context) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            String name = "PostMessage Demo";
+            String descriptionText = "A channel to send post message demo notification";
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(descriptionText);
+
+            NotificationManager notificationManager =
+                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
 }

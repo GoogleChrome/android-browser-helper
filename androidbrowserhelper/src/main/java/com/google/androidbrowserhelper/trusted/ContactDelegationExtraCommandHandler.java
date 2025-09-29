42341dc24e691d4ad742a35b99f9807778b270b4
Tomoki Sakurai <stomoki@google.com>
2025-09-29 04:44:11 +0000

    Implemented delegation for icons

--- androidbrowserhelper/src/main/java/com/google/androidbrowserhelper/trusted/ContactDelegationExtraCommandHandler.java
+++ androidbrowserhelper/src/main/java/com/google/androidbrowserhelper/trusted/ContactDelegationExtraCommandHandler.java
@@ -1,8 +1,12 @@
 package com.google.androidbrowserhelper.trusted;
 
 import android.content.ContentResolver;
+import android.content.ContentUris;
 import android.content.Context;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.RemoteException;
 import android.provider.ContactsContract;
@@ -10,6 +14,7 @@ import android.util.Log;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.browser.trusted.TrustedWebActivityCallbackRemote;
+import java.io.ByteArrayInputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,6 +23,7 @@ public class ContactDelegationExtraCommandHandler implements ExtraCommandHandler
     private static final String TAG = "stomoki ContactDeleg";
     static final String COMMAND_CHECK_CONTACT_PERMISSION = "checkContactPermission";
     private static final String COMMAND_GET_CONTACT_DATA = "getContactData";
+    private static final String COMMAND_GET_CONTACT_ICON = "getContactIcon";
 
     private static final String[] PROJECTION = {
         ContactsContract.Contacts._ID,
@@ -33,6 +39,8 @@ public class ContactDelegationExtraCommandHandler implements ExtraCommandHandler
         Bundle result = new Bundle();
         result.putBoolean(EXTRA_COMMAND_SUCCESS, false);
 
+        Bundle callbackResult = new Bundle();
+
         switch (commandName) {
             case COMMAND_CHECK_CONTACT_PERMISSION:
                 Log.d(TAG, "COMMAND_CHECK_CONTACT_PERMISSION received");
@@ -41,10 +49,23 @@ public class ContactDelegationExtraCommandHandler implements ExtraCommandHandler
                 break;
             case COMMAND_GET_CONTACT_DATA:
                 Log.d(TAG, "COMMAND_GET_CONTACT_DATA received");
-                Bundle callbackResult = new Bundle();
                 callbackResult.putStringArrayList("contactData", getContentData(context));
                 try {
                     callback.runExtraCallback(COMMAND_GET_CONTACT_DATA, callbackResult);
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
+                result.putBoolean(EXTRA_COMMAND_SUCCESS, true);
+                break;
+            case COMMAND_GET_CONTACT_ICON:
+                Log.d(TAG, "COMMAND_GET_CONTACT_ICON received");
+
+                String id = args.getString("id");
+                int iconSize = args.getInt("size");
+
+                callbackResult.putParcelable("icon", getIcon(context, id, iconSize));
+                try {
+                    callback.runExtraCallback(COMMAND_GET_CONTACT_ICON, callbackResult);
                 } catch (RemoteException e) {
                     e.printStackTrace();
                 }
@@ -77,5 +98,40 @@ public class ContactDelegationExtraCommandHandler implements ExtraCommandHandler
         }
 
         return result;
+    }
+
+    private Bitmap getIcon(Context context, String id, int iconSize) {
+        ContentResolver contentResolver = context.getContentResolver();
+
+        Uri contactUri =
+            ContentUris.withAppendedId(
+                ContactsContract.Contacts.CONTENT_URI, Long.parseLong(id));
+        Uri photoUri =
+            Uri.withAppendedPath(contactUri, ContactsContract.Contacts.Photo.CONTENT_DIRECTORY);
+        Cursor cursor =
+            contentResolver.query(
+                photoUri,
+                new String[]{ContactsContract.Contacts.Photo.PHOTO},
+                null,
+                null,
+                null);
+        if (cursor == null) {
+            return null;
+        }
+        try {
+            if (cursor.moveToFirst()) {
+                byte[] data = cursor.getBlob(0);
+                if (data != null) {
+                    Bitmap icon = BitmapFactory.decodeStream(new ByteArrayInputStream(data));
+                    return iconSize > 0
+                        ? Bitmap.createScaledBitmap(
+                        icon, iconSize, iconSize, true)
+                        : icon;
+                }
+            }
+        } finally {
+            cursor.close();
+        }
+        return null;
     }
 }

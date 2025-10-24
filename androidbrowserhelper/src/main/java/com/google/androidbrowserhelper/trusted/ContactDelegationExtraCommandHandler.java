46c01d7b1940323268ea95402d83a8fc7c6cb11d
Tomoki Sakurai <stomoki@google.com>
2025-10-24 07:30:35 +0000

    Change the method to pass icons

--- androidbrowserhelper/src/main/java/com/google/androidbrowserhelper/trusted/ContactDelegationExtraCommandHandler.java
+++ androidbrowserhelper/src/main/java/com/google/androidbrowserhelper/trusted/ContactDelegationExtraCommandHandler.java
@@ -1,8 +1,12 @@
 package com.google.androidbrowserhelper.trusted;
 
+import android.content.ComponentName;
 import android.content.ContentResolver;
 import android.content.ContentUris;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -14,17 +18,25 @@ import android.util.Log;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.browser.trusted.TrustedWebActivityCallbackRemote;
+import androidx.core.content.FileProvider;
 import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class ContactDelegationExtraCommandHandler implements ExtraCommandHandler {
 
     private static final String TAG = "stomoki ContactDeleg";
     static final String COMMAND_CHECK_CONTACT_PERMISSION = "checkContactPermission";
-    private static final String COMMAND_GET_CONTACT_DATA = "getContactData";
-    private static final String COMMAND_GET_CONTACT_ICON = "getContactIcon";
+    public static final String COMMAND_FETCH_CONTACTS = "fetchContacts";
+    public static final String COMMAND_FETCH_CONTACT_ICON = "fetchContactIcon";
+
+    private static final String FOLDER_NAME = "contact_icons";
 
     private static final String[] PROJECTION = {
         ContactsContract.Contacts._ID,
@@ -32,11 +44,60 @@ public class ContactDelegationExtraCommandHandler implements ExtraCommandHandler
         ContactsContract.Contacts.DISPLAY_NAME_PRIMARY,
     };
 
+    private static final ExecutorService sExecutorService = Executors.newCachedThreadPool();
+
+    private String mProviderPackage;
+    private String mFileProviderAuthority;
+
+    private File mDir;
+
+    private synchronized void init(Context context) {
+        if (mProviderPackage != null) {
+            return;
+        }
+
+        mDir = new File(context.getFilesDir(), FOLDER_NAME);
+
+        if (!mDir.exists()) {
+            boolean mkDirSuccessful = mDir.mkdir();
+            if (!mkDirSuccessful) {
+                Log.e(TAG, "Failed to create a directory for storing a splash image");
+            }
+        }
+
+        PackageManager packageManager = context.getPackageManager();
+
+        Log.d(TAG, "package = " + context.getPackageName());
+
+        TwaProviderPicker.Action action = TwaProviderPicker.pickProvider(packageManager);
+        mProviderPackage = action.provider;
+
+        Log.d(TAG, "provider = " + mProviderPackage);
+
+        ComponentName componentName = new ComponentName(context, LauncherActivity.class);
+
+        try {
+            mFileProviderAuthority =
+                packageManager
+                    .getActivityInfo(componentName, PackageManager.GET_META_DATA)
+                    .metaData
+                    .getString("android.support.customtabs.trusted.FILE_PROVIDER_AUTHORITY");
+        } catch (NameNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        Log.d(TAG, "authority = " + mFileProviderAuthority);
+    }
 
     @NonNull
     @Override
-    public Bundle handleExtraCommand(Context context, String commandName, Bundle args,
+    public Bundle handleExtraCommand(
+        Context context,
+        String commandName,
+        Bundle args,
         @Nullable TrustedWebActivityCallbackRemote callback) {
+        init(context);
+
         Bundle result = new Bundle();
         result.putBoolean(EXTRA_COMMAND_SUCCESS, false);
 
@@ -48,34 +109,71 @@ public class ContactDelegationExtraCommandHandler implements ExtraCommandHandler
                 ContactPermissionRequestActivity.requestContactPermisson(context, callback);
                 result.putBoolean(EXTRA_COMMAND_SUCCESS, true);
                 break;
-            case COMMAND_GET_CONTACT_DATA:
-                Log.d(TAG, "COMMAND_GET_CONTACT_DATA received");
+            case COMMAND_FETCH_CONTACTS:
+                Log.d(TAG, "COMMAND_FETCH_CONTACTS received");
                 boolean includeNames = args.getBoolean("includeNames");
                 boolean includeEmails = args.getBoolean("includeEmails");
                 boolean includeTel = args.getBoolean("includeTel");
                 boolean includeAddresses = args.getBoolean("includeAddresses");
-                callbackResult.putParcelableArrayList("contacts",
-                    getAllContacts(context, includeNames, includeEmails, includeTel,
-                        includeAddresses));
-                try {
-                    callback.runExtraCallback(COMMAND_GET_CONTACT_DATA, callbackResult);
-                } catch (RemoteException e) {
-                    e.printStackTrace();
-                }
+
+                sExecutorService.submit(
+                    () -> {
+                        callbackResult.putParcelableArrayList(
+                            "contacts",
+                            getAllContacts(
+                                context, includeNames, includeEmails, includeTel,
+                                includeAddresses));
+                        try {
+                            callback.runExtraCallback(COMMAND_FETCH_CONTACTS, callbackResult);
+                        } catch (RemoteException e) {
+                            e.printStackTrace();
+                        }
+                    });
                 result.putBoolean(EXTRA_COMMAND_SUCCESS, true);
                 break;
-            case COMMAND_GET_CONTACT_ICON:
-                Log.d(TAG, "COMMAND_GET_CONTACT_ICON received");
+            case COMMAND_FETCH_CONTACT_ICON:
+                Log.d(TAG, "COMMAND_FETCH_CONTACT_ICON received");
 
                 String id = args.getString("id");
                 int iconSize = args.getInt("size");
 
-                callbackResult.putParcelable("icon", getIcon(context, id, iconSize));
-                try {
-                    callback.runExtraCallback(COMMAND_GET_CONTACT_ICON, callbackResult);
-                } catch (RemoteException e) {
-                    e.printStackTrace();
-                }
+                sExecutorService.submit(
+                    () -> {
+                        Bitmap icon = getIcon(context, id, iconSize);
+
+                        if (icon != null) {
+                            File file = new File(mDir, "contact_icon_" + id);
+
+                            Log.d(TAG, "file = " + file.getAbsolutePath());
+
+                            try (FileOutputStream os = new FileOutputStream(file)) {
+                                icon.compress(Bitmap.CompressFormat.PNG, 100, os);
+                                os.flush();
+                            } catch (IOException e) {
+                                throw new RuntimeException(e);
+                            }
+
+                            Log.d(TAG, "file written");
+
+                            synchronized (context) {
+                                Uri uri = FileProvider.getUriForFile(context,
+                                    mFileProviderAuthority, file);
+
+                                Log.d(TAG, "uri = " + uri);
+
+                                context.grantUriPermission(
+                                    mProviderPackage, uri, Intent.FLAG_GRANT_READ_URI_PERMISSION);
+
+                                callbackResult.putParcelable("icon", uri);
+                            }
+                        }
+                        try {
+                            callback.runExtraCallback(COMMAND_FETCH_CONTACT_ICON, callbackResult);
+                        } catch (RemoteException e) {
+                            e.printStackTrace();
+                        }
+                    });
+
                 result.putBoolean(EXTRA_COMMAND_SUCCESS, true);
                 break;
             default:
@@ -86,8 +184,12 @@ public class ContactDelegationExtraCommandHandler implements ExtraCommandHandler
         return result;
     }
 
-    private Map<String, ArrayList<String>> getDetails(ContentResolver contentResolver,
-        Uri source, String idColumn, String dataColumn, String sortOrder) {
+    private Map<String, ArrayList<String>> getDetails(
+        ContentResolver contentResolver,
+        Uri source,
+        String idColumn,
+        String dataColumn,
+        String sortOrder) {
         Map<String, ArrayList<String>> map = new HashMap<>();
 
         Cursor cursor = contentResolver.query(source, null, null, null, sortOrder);
@@ -160,8 +262,7 @@ public class ContactDelegationExtraCommandHandler implements ExtraCommandHandler
             String formattedAddress =
                 cursor.getString(
                     cursor.getColumnIndexOrThrow(
-                        ContactsContract.CommonDataKinds.StructuredPostal
-                            .FORMATTED_ADDRESS));
+                        ContactsContract.CommonDataKinds.StructuredPostal.FORMATTED_ADDRESS));
             String postcode =
                 cursor.getString(
                     cursor.getColumnIndexOrThrow(
@@ -198,14 +299,18 @@ public class ContactDelegationExtraCommandHandler implements ExtraCommandHandler
         return map;
     }
 
-    public ArrayList<Bundle> getAllContacts(Context context, boolean includeNames,
+    public ArrayList<Bundle> getAllContacts(
+        Context context,
+        boolean includeNames,
         boolean includeEmails,
-        boolean includeTel, boolean includeAddresses) {
+        boolean includeTel,
+        boolean includeAddresses) {
         ContentResolver contentResolver = context.getContentResolver();
 
         Map<String, ArrayList<String>> emailMap =
             includeEmails
-                ? getDetails(contentResolver,
+                ? getDetails(
+                contentResolver,
                 ContactsContract.CommonDataKinds.Email.CONTENT_URI,
                 ContactsContract.CommonDataKinds.Email.CONTACT_ID,
                 ContactsContract.CommonDataKinds.Email.DATA,
@@ -217,7 +322,8 @@ public class ContactDelegationExtraCommandHandler implements ExtraCommandHandler
 
         Map<String, ArrayList<String>> phoneMap =
             includeTel
-                ? getDetails(contentResolver,
+                ? getDetails(
+                contentResolver,
                 ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
                 ContactsContract.CommonDataKinds.Phone.CONTACT_ID,
                 ContactsContract.CommonDataKinds.Phone.DATA,
@@ -250,16 +356,14 @@ public class ContactDelegationExtraCommandHandler implements ExtraCommandHandler
 
         ArrayList<Bundle> contacts = new ArrayList<>(cursor.getCount());
         do {
-            String id =
-                cursor.getString(cursor.getColumnIndexOrThrow(ContactsContract.Contacts._ID));
+            String id = cursor.getString(
+                cursor.getColumnIndexOrThrow(ContactsContract.Contacts._ID));
             String name =
                 cursor.getString(
-                    cursor.getColumnIndexOrThrow(
-                        ContactsContract.Contacts.DISPLAY_NAME_PRIMARY));
+                    cursor.getColumnIndexOrThrow(ContactsContract.Contacts.DISPLAY_NAME_PRIMARY));
             ArrayList<String> email = emailMap != null ? emailMap.get(id) : null;
             ArrayList<String> tel = phoneMap != null ? phoneMap.get(id) : null;
-            ArrayList<Bundle> address =
-                addressMap != null ? addressMap.get(id) : null;
+            ArrayList<Bundle> address = addressMap != null ? addressMap.get(id) : null;
 
             if (includeNames || email != null || tel != null || address != null) {
                 Bundle contact = new Bundle();
@@ -282,17 +386,12 @@ public class ContactDelegationExtraCommandHandler implements ExtraCommandHandler
         ContentResolver contentResolver = context.getContentResolver();
 
         Uri contactUri =
-            ContentUris.withAppendedId(
-                ContactsContract.Contacts.CONTENT_URI, Long.parseLong(id));
+            ContentUris.withAppendedId(ContactsContract.Contacts.CONTENT_URI, Long.parseLong(id));
         Uri photoUri =
             Uri.withAppendedPath(contactUri, ContactsContract.Contacts.Photo.CONTENT_DIRECTORY);
         Cursor cursor =
             contentResolver.query(
-                photoUri,
-                new String[]{ContactsContract.Contacts.Photo.PHOTO},
-                null,
-                null,
-                null);
+                photoUri, new String[]{ContactsContract.Contacts.Photo.PHOTO}, null, null, null);
         if (cursor == null) {
             return null;
         }
@@ -301,9 +400,7 @@ public class ContactDelegationExtraCommandHandler implements ExtraCommandHandler
                 byte[] data = cursor.getBlob(0);
                 if (data != null) {
                     Bitmap icon = BitmapFactory.decodeStream(new ByteArrayInputStream(data));
-                    return iconSize > 0
-                        ? Bitmap.createScaledBitmap(
-                        icon, iconSize, iconSize, true)
+                    return iconSize > 0 ? Bitmap.createScaledBitmap(icon, iconSize, iconSize, true)
                         : icon;
                 }
             }
